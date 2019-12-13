0fc726ce38c40289a340bacc943d284f35698b75
Ivens <ivensjorisnbacon@gmail.com>
2019-12-13 03:12:41 -0300

    added graphql json server

--- express/server.js
+++ express/server.js
@@ -1,9 +1,11 @@
-'use strict';
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
+const jsonGraphqlExpress = require('json-graphql-server');
+const got = require('got');
+
+const app = express();
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -11,8 +13,23 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use('/graphql', async (req, res, next) => {
+  const url = "https://www.jsonstore.io/0e80d3317ae8f2ecc87eb87e785b033cb335ad7ecfd23dab6456c44f5494e446"
+  let data = {};
+
+  const body = await got(url).json();
+  data = body.result
+  jsonGraphqlExpress.default(data)(req, res, next)
+
+  setTimeout(async () => {
+    const body = await got.post(url, {
+      json: data
+    }).json();
+
+    console.log(body);
+  }, 2000);
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
