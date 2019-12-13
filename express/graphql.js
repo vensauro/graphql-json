09e7f6a1ef2677decbfa4ebee7b6a2219d41d553
Ivens <ivensjorisnbacon@gmail.com>
2019-12-13 03:42:02 -0300

    fix debug

--- express/graphql.js
+++ express/graphql.js
@@ -7,11 +7,6 @@ const got = require('got');
 const app = express();
 
 const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
 
 router.use('/', async (req, res, next) => {
   const url = process.env.URL
